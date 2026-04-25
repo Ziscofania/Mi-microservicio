726dd8b55e6286999330b9662ca74ee530591732
Ziscofania <cesar13luna14@gmail.com>
2026-04-24 20:42:20 -0500

    conexion con PG

--- src/index.js
+++ src/index.js
@@ -1,20 +1,39 @@
 import express from "express";
 import cors from "cors";
-    
+import pkg from "pg";
+
+const { Pool } = pkg;
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 4000;
 
-// Endpoint de prueba
-app.get("/api/users", (req, res) => {
-    res.json([
-        { id: 1, name: "Cesar" },
-        { id: 2, name: "Camilo" }
-    ]);
+//  conexión a PostgreSQL (Railway usa DATABASE_URL)
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // necesario para Railway
+  },
+});
+
+// probar conexión al iniciar
+pool.connect()
+  .then(() => console.log("Conectado a PostgreSQL"))
+  .catch(err => console.error("Error conexión DB:", err));
+
+// Endpoint real usando DB
+app.get("/api/users", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM users");
+    res.json(result.rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Error al obtener usuarios" });
+  }
 });
 
 app.listen(PORT, () => {
-    console.log(`User Service running on port ${PORT}`);
+  console.log(`User Service running on port ${PORT}`);
 });
